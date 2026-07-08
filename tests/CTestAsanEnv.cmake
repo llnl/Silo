e4d9163bc47d74cb3ee27645b6c34f2fdf072a97
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2026-07-08 14:26:05 -0700

    Handle address sanitizer on macOS

--- tests/CTestAsanEnv.cmake
+++ tests/CTestAsanEnv.cmake
@@ -1,2 +1,6 @@
 # This file is included by CTest at test time (not configure time)
+if(APPLE)
+    set(ENV{DYLD_INSERT_LIBRARIES} "/Library/Developer/CommandLineTools/usr/lib/clang/17/lib/darwin/libclang_rt.asan_osx_dynamic.dylib")
+    set(ENV{MallocNanoZone} "0")
+endif()
 set(ENV{ASAN_OPTIONS} "detect_leaks=1:halt_on_error=1:malloc_context_size=50:fast_unwind_on_malloc=0:detect_stack_use_after_return=1")
