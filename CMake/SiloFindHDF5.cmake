108db06c564ab9e1d496604ee98b36857ab770f5
Mark C. Miller <5720676+markcmiller86@users.noreply.github.com>
2026-07-17 18:43:55 -0700

    fix cmake logic

--- CMake/SiloFindHDF5.cmake
+++ CMake/SiloFindHDF5.cmake
@@ -102,7 +102,7 @@ if(HDF5_FOUND)
     if(WIN32)
         # DLL may also be needed by testing infrastructure
         get_target_property(HDF5_DLL ${HDF5_C_LIBRARIES} IMPORTED_LOCATION_RELEASE )
-        if(SILO_ENABLE_SILEX OR SILO_ENABLE_BROWSER)
+        if(HDF5_DLL AND SILO_ENABLE_SILEX OR SILO_ENABLE_BROWSER)
             install(FILES ${HDF5_DLL} DESTINATION ${CMAKE_INSTALL_BINDIR}
                     PERMISSIONS OWNER_READ OWNER_WRITE
                                 GROUP_READ GROUP_WRITE
